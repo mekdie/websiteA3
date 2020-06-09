2131acb2d5ccd3ca525c25147a346c3b758ecd0f
mcdonylee <mcdonyleee@gmail.com>
2020-06-09 22:03:18 +1000

    validated

--- assets/js/sweetAlert.js
+++ assets/js/sweetAlert.js
@@ -5,6 +5,7 @@ function submitBtn() {
         icon: 'success',
         title: 'Submitted!',
         text: 'Thank you for contacting us',
-        text: 'We will get in touch with you in 2 business days'
+        text: 'We will get in touch with you in 2 business days',
+        showCloseButton: true,
     })
 }
