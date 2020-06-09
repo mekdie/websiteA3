56f601ff4ad15bc3f93ea4fbf5c6b84e8cbc9a84
mcdonylee <mcdonyleee@gmail.com>
2020-06-09 21:26:06 +1000

    updated sweet alert

---
+++ assets/js/sweetAlert.js
@@ -0,0 +1,10 @@
+submitBtn();
+
+function submitBtn() {
+    Swal.fire({
+        icon: 'success',
+        title: 'Submitted!',
+        text: 'Thank you for contacting us',
+        text: 'We will get in touch with you in 2 business days'
+    })
+}
